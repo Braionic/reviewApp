c5b4893bddf06ba0b2573e4fbc64cc99cf048ee9
Braionic <trustedward@gmail.com>
2023-12-01 22:32:42 +0100

    added you form validation package for validation

--- component/AddFrorm.jsx
+++ component/AddFrorm.jsx
@@ -1,16 +1,25 @@
 import { View, Text, TextInput, StyleSheet } from 'react-native'
 import React from 'react'
-import { Form, Formik } from 'formik'
+import { Form, Formik, validateYupSchema } from 'formik'
 import { Button } from 'react-native-elements'
 import { globalStyles } from '../styles/global'
+import * as yup from 'yup'
 
 export default function AddFrorm({handleFormData}) {
+
+  const formValidation = yup.object({
+    title: yup.string().required().min(4),
+    name: yup.string().required().min(2),
+    rating: yup.string().required().test('is-less-or-greater', 'value must be < 6 & > 0', (value)=>{
+      return parseInt(value) > 0 && parseInt(value) < 6
+  })
+  })
   return (
     <View style={styles.container}>
      <Formik onSubmit={(values, actions)=>(
         actions.resetForm(),
         handleFormData(values)
-     )} initialValues={{name: '', title: '', body: '', rating: '', id: ''}}>
+     )} initialValues={{name: '', title: '', body: '', rating: '', id: ''}} validationSchema={formValidation}>
         {(props)=>(
             <View>
             <TextInput
